6cf02a1fda3b8f17067e20bebd585ab74a0da679
mr-stamp <nathanael.stamp@youngsvilleacademy.org>
2026-05-07 11:01:34 -0400

    Update listmaker.js

--- wdsub/listmaker.js
+++ wdsub/listmaker.js
@@ -82,13 +82,23 @@ function uncheckItem(elem) {
 
 function loadOldList() {
   cookieArr = document.cookie.split(";");
-  cookieArr.forEach((_item) => {
+  
+  for (let _i = 0; _i < cookieArr.length; _i++) {
+    let _item = cookieArr[_i];
     let _key = _item.split("=")[0];
     let _value = _item.split("=")[1];
     if (_key.substr(0,4) == "item" && typeof _value !== "undefined") {
       autoAddItem(_value);
     }
-  });
+  }
+  
+  // cookieArr.forEach((_item) => {
+  //   let _key = _item.split("=")[0];
+  //   let _value = _item.split("=")[1];
+  //   if (_key.substr(0,4) == "item" && typeof _value !== "undefined") {
+  //     autoAddItem(_value);
+  //   }
+  // });
 }
 
 function saveItem(str) {
